d6239eddf88c2906d68ed18cc7630ef039d11049
Tommy Chen <tommy351@gmail.com>
2021-04-07 11:45:11 +0800

    Update svgToJsx function

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,5 @@ export function svgToJsx(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   path: string
 ): SVGFactory {
-  throw new Error(
-    "Do not use svgToJsx directly. Please make sure ts-transformer-svg-jsx is set up properly."
-  );
+  return () => null as any;
 }
